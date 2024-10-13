994bdd2d6574ae3d115ea5db09c772bf35626d90
slimyzockt77 <slimyzockt77@gmail.com>
2024-10-13 16:51:02 +0200

    v0.1.1

--- main.go
+++ main.go
@@ -5,16 +5,45 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"os/signal"
+	"strconv"
 )
 
 type Config struct {
 	GitRepoDir string   `json:"git_repo_dir"`
-	ID         int      `json:"id"`
 	Dotfiles   []string `json:"dotfiles"`
 }
 
+var config Config
+var id int
+
 func main() {
-	var config Config
+	interrupt_chan := make(chan os.Signal, 1)
+	signal.Notify(interrupt_chan, os.Interrupt)
+	go func() {
+		<-interrupt_chan
+
+		err := os.WriteFile(config.GitRepoDir+"id", []byte(strconv.Itoa(id-1)), 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		os.Exit(1)
+	}()
+
+	app()
+}
+
+func app() {
+	if _, err := os.Stat("./config.json"); os.IsNotExist(err) {
+		if err != nil {
+			b, err := json.Marshal(&config)
+			if err != nil {
+				log.Fatal(err)
+			}
+			os.WriteFile("./config.json", b, 0644)
+		}
+	}
 
 	config_file, err := os.ReadFile("./config.json")
 	if err != nil {
@@ -24,15 +53,6 @@ func main() {
 	err = json.Unmarshal(config_file, &config)
 	if err != nil {
 		log.Fatal(err, " Cant convert to struct")
-	}
-
-	if _, err := os.Stat("./config.json"); os.IsNotExist(err) {
-		if err != nil {
-			log.Fatal(err, "Error")
-		}
-
-		exec.Command("bash", "-c", `
-			`).Run()
 	}
 
 	for _, path := range config.Dotfiles {
@@ -45,21 +65,51 @@ func main() {
 			log.Fatal(err)
 		}
 
-		os.WriteFile("./dotfiles/"+stat.Name(), file, 0644)
+		err = os.WriteFile(config.GitRepoDir+stat.Name(), file, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+
 	}
 
-	log.Print(config.ID)
+	id_data, err := os.ReadFile(config.GitRepoDir + "id")
 
-	cmd := exec.Command("bash", "-c", `
-		cd ./dotfiles/
-		git add .
-		git diff
-		git diff
+	if os.IsNotExist(err) {
+		err := os.WriteFile(config.GitRepoDir+"id", []byte("0"), 0644)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		id = 0
+	} else if err == nil {
+		new_id, err := strconv.Atoi(string(id_data))
+		if err != nil {
+			log.Fatal(err)
+		}
+		id = new_id + 1
+
+	} else {
+		log.Fatal(err)
+	}
+
+	log.Print("New id: ", id)
+
+	cmd := exec.Command("bash", "-c",
+		"cd "+config.GitRepoDir+`
+		git diff -U0
+		git commit -am "`+
+			strconv.Itoa(id)+`"
 		git push
-	`)
+		`)
 
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+
+	err = os.WriteFile(config.GitRepoDir+"id", []byte(strconv.Itoa(id)), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	cmd.Run()
 }
